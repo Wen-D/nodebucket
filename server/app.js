39c516b1924e0b026e37329cb7ef08c959d219f4
Wendy Leon <pixelmamma@gmail.com>
2021-03-20 12:09:39 -0400

    Sprint 1

--- server/app.js
+++ server/app.js
@@ -1,19 +1,30 @@
+/***
+** Title: app.js
+** Author: Wendy Leon
+** Date: March 18 2021
+** Description: API - findEmployeeById Sprint 1
+ ***/
 /**
  * Require statements
  */
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
-const bodyParser = require('body-parser');                        
+///////////////////const bodyParser = require('body-parser');
 const path = require('path');
 const mongoose = require('mongoose');
+// const emp model access schema
+//const Employee = require ('./db-models/employee'); no longer needed because of API
+
+const EmployeeApi = require('./routes/employee-route'); // employee API - import
+
 
 /**
  * App configurations
  */
 let app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({'extended': true}));
+app.use(express.json());
+app.use(express.urlencoded({'extended': true}));
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../dist/nodebucket')));
 app.use('/', express.static(path.join(__dirname, '../dist/nodebucket')));
@@ -21,10 +32,10 @@ app.use('/', express.static(path.join(__dirname, '../dist/nodebucket')));
 /**
  * Variables
  */
-const port = 3000; // server port
+ const port = process.env.PORT || 3000; // server port
 
-// TODO: This line will need to be replaced with your actual database connection string
-const conn = 'mongodb+srv://superadmin:s3cret@cluster0-lujih.mongodb.net/nodebucket?retryWrites=true&w=majority';
+// mongo connection to db string
+const conn = 'mongodb+srv://nodebucket_user:admin@buwebdev-cluster-1.oqsoi.mongodb.net/nodebucket?retryWrites=true&w=majority';
 
 /**
  * Database connection
@@ -32,7 +43,8 @@ const conn = 'mongodb+srv://superadmin:s3cret@cluster0-lujih.mongodb.net/nodebuc
 mongoose.connect(conn, {
   promiseLibrary: require('bluebird'),
   useUnifiedTopology: true,
-  useNewUrlParser: true
+  useNewUrlParser: true,
+  useCreateIndex: true
 }).then(() => {
   console.debug(`Connection to the database instance was successful`);
 }).catch(err => {
@@ -42,10 +54,12 @@ mongoose.connect(conn, {
 /**
  * API(s) go here...
  */
+ app.use('/api/employees', EmployeeApi);
+
 
 /**
  * Create and start server
  */
-http.createServer(app).listen(port, function() {
+ http.createServer(app).listen(port, function() {
   console.log(`Application started and listening on port: ${port}`)
 }); // end http create server function
